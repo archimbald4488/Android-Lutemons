faed31e9de6ec44626729ebee0790142a9860fde
Ari Holahan <holahan.ari@gmail.com>
2025-04-22 16:16:00 +0300

    Added views

--- app/src/main/java/oliot/lutemons/activities/MainActivity.java
+++ app/src/main/java/oliot/lutemons/activities/MainActivity.java
@@ -2,12 +2,25 @@ package oliot.lutemons.activities;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
 import oliot.lutemons.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment);
+
+        NavController navController = navHostFragment.getNavController();
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
+        NavigationUI.setupWithNavController(bottomNav, navController);
     }
 }
