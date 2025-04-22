f140f75ea510027c0f32cb0a7f072081c54daf18
MoShimer96 <135559911+MoShimer96@users.noreply.github.com>
2025-04-22 16:09:10 +0300

    Test commit. Nothing of relevance was added. Send me a messge via Telegram when you can.

--- app/src/main/java/oliot/lutemons/activities/MainActivity.java
+++ app/src/main/java/oliot/lutemons/activities/MainActivity.java
@@ -3,6 +3,13 @@ package oliot.lutemons.activities;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import oliot.lutemons.R;
+// Was able to clone the repo.
+// Everything seems to be working fine.
+// Added these comments to see that commit and push are working.
+// The app does not run, because there is no activity_main xml?
+
+// I created a seperate branch for local testing.
+// You can decline this no biggie. It made some local additions to gradle.xml and misc.xml for some reason.
 
 public class MainActivity extends AppCompatActivity {
     @Override
