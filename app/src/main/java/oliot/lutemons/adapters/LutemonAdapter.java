67fdee27e830aa8b680701baa1fe191271e09d49
Ari Holahan <holahan.ari@gmail.com>
2025-04-23 11:43:46 +0300

    Updated the home fragment and training fragment, added button to send lutemon to training

--- app/src/main/java/oliot/lutemons/adapters/LutemonAdapter.java
+++ app/src/main/java/oliot/lutemons/adapters/LutemonAdapter.java
@@ -3,8 +3,10 @@ package oliot.lutemons.adapters;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -12,6 +14,7 @@ import java.util.ArrayList;
 
 import oliot.lutemons.R;
 import oliot.lutemons.models.Lutemon;
+import oliot.lutemons.storage.Storage;
 
 public class LutemonAdapter extends RecyclerView.Adapter<LutemonAdapter.ViewHolder> {
     private ArrayList<Lutemon> lutemons;
@@ -23,12 +26,14 @@ public class LutemonAdapter extends RecyclerView.Adapter<LutemonAdapter.ViewHold
     public static class ViewHolder extends RecyclerView.ViewHolder {
         TextView nameView, statsView;
         ImageView imageView;
+        Button trainButton;
 
         public ViewHolder(View itemView) {
             super(itemView);
             nameView = itemView.findViewById(R.id.lutemon_name);
             statsView = itemView.findViewById(R.id.lutemon_stats);
             imageView = itemView.findViewById(R.id.lutemon_image);
+            trainButton = itemView.findViewById(R.id.btn_send_to_training);
         }
     }
 
@@ -44,7 +49,14 @@ public class LutemonAdapter extends RecyclerView.Adapter<LutemonAdapter.ViewHold
         Lutemon lutemon = lutemons.get(position);
         holder.nameView.setText(lutemon.getName());
         holder.statsView.setText("HP: " + lutemon.getCurrentHealth());
-        //holder.imageView.setImageResource(lutemon.getImageId()); ADD IMAGES TO LUTEMONS FIRST
+        //holder.imageView.setImageResource(lutemon.getImageId()); uncomment after adding real images
+
+        holder.trainButton.setOnClickListener(v -> {
+            Storage.getInstance().moveToTraining(lutemon.getId());
+            lutemons.remove(position);  // remove from current view if needed
+            notifyItemRemoved(position);
+            Toast.makeText(holder.itemView.getContext(), lutemon.getName() + " sent to training!", Toast.LENGTH_SHORT).show();
+        });
     }
 
     @Override
--- app/src/main/java/oliot/lutemons/fragments/HomeFragment.java
+++ app/src/main/java/oliot/lutemons/fragments/HomeFragment.java
@@ -5,19 +5,33 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 import oliot.lutemons.R;
+import oliot.lutemons.adapters.LutemonAdapter;
+import oliot.lutemons.storage.Storage;
 
 public class HomeFragment extends Fragment {
 
-    @Nullable
+    private RecyclerView recyclerView;
+    private LutemonAdapter adapter;
+
+    public HomeFragment() {}
+
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        recyclerView = view.findViewById(R.id.recyclerViewHome);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        adapter = new LutemonAdapter(new ArrayList<>(Storage.getInstance().getHomeLutemons().values()));
+        recyclerView.setAdapter(adapter);
+
+        return view;
     }
 }
