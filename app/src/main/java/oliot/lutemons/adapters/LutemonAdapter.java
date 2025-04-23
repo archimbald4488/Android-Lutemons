f2321f32c8419f2ed8a3e35ef31a4762a91cf412
Ari Holahan <holahan.ari@gmail.com>
2025-04-23 13:51:21 +0300

    Added send home button and ensured training works when clicking on lutemon

--- app/src/main/java/oliot/lutemons/adapters/LutemonAdapter.java
+++ app/src/main/java/oliot/lutemons/adapters/LutemonAdapter.java
@@ -58,7 +58,11 @@ public class LutemonAdapter extends RecyclerView.Adapter<LutemonAdapter.ViewHold
         Lutemon lutemon = lutemons.get(position);
         String lutemonName = lutemon.getName() + " (" + lutemon.getType() + ")";
         holder.nameView.setText(lutemonName);
-        holder.statsView.setText("HP: " + lutemon.getCurrentHealth());
+        String statsText = "Exp: " + lutemon.getExperience() +
+                " | HP: " + lutemon.getCurrentHealth() +
+                " | ATK: " + lutemon.getAttack() +
+                " | DEF: " + lutemon.getDefense();
+        holder.statsView.setText(statsText);
         //holder.imageView.setImageResource(lutemon.getImageId()); uncomment after adding real images
 
         // Set click listener for "Send to Training"
